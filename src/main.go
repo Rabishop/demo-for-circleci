8aa9c668512a28d455cd8766b4f8ba323af5bc36
Rabishop <50069587+Rabishop@users.noreply.github.com>
2023-06-13 02:55:07 +0900

    Update main.go

--- src/main.go
+++ src/main.go
@@ -3,22 +3,25 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"net/http"
 	"time"
 )
 
 func main() {
+	http.HandleFunc("/", helloHandler)
+	http.ListenAndServe(":8080", nil)
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
 	// 获取当前时间
 	currentTime := time.Now()
-
-	// 输出当前时间
-	fmt.Println("Current time:", currentTime)
 
 	// 生成一个介于1到100之间的随机数
 	randomNumber := rand.Intn(100) + 1
 
-	// 输出随机数
-	fmt.Println("Random number:", randomNumber)
+	// 构造响应消息
+	message := fmt.Sprintf("Current time: %s\nRandom number: %d\nHello, World!", currentTime, randomNumber)
 
-	// 输出 "Hello, World!"
-	fmt.Println("Hello, World!")
+	// 将响应消息写入响应写入器
+	fmt.Fprint(w, message)
 }
